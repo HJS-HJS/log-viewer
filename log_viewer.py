19375bb690099b0e1431f2dd6a76059d9357304f
Jong Soo Han <jongsoo@g.skku.edu>
2025-11-16 18:34:26 +0900

    Update and rename main.py to log_viewer.py

--- log_viewer.py
+++ log_viewer.py
@@ -1,11 +1,20 @@
-# main.py
 import sys
+import os
 from PyQt5.QtWidgets import QApplication
 from widget.main_window import MainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    
+
+    if getattr(sys, 'frozen', False):
+        # 1. PyInstaller로 빌드된 .exe 파일일 때
+        # sys.executable은 .exe 파일의 경로입니다.
+        base_path = os.path.dirname(sys.executable)
+    else:
+        # 2. .py 스크립트로 직접 실행할 때
+        # __file__은 이 .py 파일의 경로입니다.
+        base_path = os.path.dirname(os.path.abspath(__file__))
+
     # 다크 모드 스타일을 앱 전체에 적용
     app.setStyleSheet("""
     QWidget { 
@@ -36,12 +45,12 @@ if __name__ == "__main__":
     /* 'i' 체크박스만 작게 만들기 */
     QCheckBox[objectName="case_i_cb"] { font-size: 9pt; }
     QCheckBox[objectName="case_i_cb"]::indicator { width: 12px; height: 12px; }
-    QScrollBar:vertical { background: #1e1e1e; width: 12px; }
+    QScrollBar:vertical { background: #1e1e1e; width: 12px; min-height: 45px;}
     QScrollBar::handle:vertical { background: #e0e0e0; }
     QSplitter::handle { background-color: #444; }
     QSplitter::handle:horizontal { width: 2px; }
     """)
     
-    viewer = MainWindow()
+    viewer = MainWindow(base_path)
     viewer.show()
     sys.exit(app.exec_())
